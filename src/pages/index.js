39a56b353a397cdd244d01d5751fb40bcef5fc57
Siddhant Patil <sidomatic11@gmail.com>
2020-06-22 14:13:37 -0700

    First Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello!</h1>
+    <p>This is where I will build my portfolio.</p>
+    <p>Let's do this!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    {/* <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
 
