a7d2e6555f238a5e94955716bdb0725f7b330a26
Siddhant Patil <sidomatic11@gmail.com>
2020-06-22 19:40:59 -0700

    repo rename test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Siddhant Patil, UX Designer`,
+    title: `Siddhant Patil`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
