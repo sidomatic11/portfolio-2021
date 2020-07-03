4380835406b8af73340be471d89feed714e563a3
Siddhant Patil <sidomatic11@gmail.com>
2020-07-02 23:04:45 -0700

    adding pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,32 @@
 import React from "react"
 // import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+// import Layout from "../components/layout"
+import Menu from "../components/menu"
 // import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    {/* <SEO title="About" /> */}
+// const AboutPage = () => (
+//   <Layout>
+//     {/* <SEO title="About" /> */}
+    
+//     <h2>About Me</h2>
+//     <p>&lt;Picture&gt;</p>
+//     <p>Pitch</p>
+//     <p>Story</p>
+//     <p>Social links</p>
+//     <p>Testimonials</p>
+//     <p>Tags that describe me</p>
+//     <p>&nbsp;</p>
+    
+//   </Layout>
+// )
+
+
+
+const AboutPage = () => (
+  <>
+
+    <Menu/>
     
     <h2>About Me</h2>
     <p>&lt;Picture&gt;</p>
@@ -16,8 +36,8 @@ const SecondPage = () => (
     <p>Testimonials</p>
     <p>Tags that describe me</p>
     <p>&nbsp;</p>
-    
-  </Layout>
+
+  </>
 )
 
-export default SecondPage
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "./global.scss" 
-import logo from "./../images/logo.svg"
+// import logo from "./../images/logo.svg"
+import Menu from "./../components/menu"
 
 require("typeface-open-sans")
 // import Layout from "../components/layout"
@@ -21,17 +22,13 @@ require("typeface-open-sans")
 const IndexPage = () => (
   <>
     <div className="homePage">
+
       <div className="headline">
         Siddhant is a <b className="dark">cross-disciplinary designer</b>. He cares deeply about making things as <i className="dark">efficient</i> as possible.
       </div>
       
-      <div className="mainMenu">
-        <div className=" menuLabel right">work</div>
-        <div className=" image is-48x48">
-          <img src={logo} alt="siddhant patil"></img>
-        </div>
-        <div className=" menuLabel">about</div>
-      </div>
+      <Menu/>
+
     </div>
 
   </>
